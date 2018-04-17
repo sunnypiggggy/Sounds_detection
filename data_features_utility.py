7b31b11ac988c60dcdaa36539f29c8733a577cef
sunnypiggggy <armhawk6@gmail.com>
2018-04-17 22:45:01 +0800

    gogogo

--- data_features_utility.py
+++ data_features_utility.py
@@ -7,7 +7,6 @@ import glob
 import sys
 import pickle
 import soundfile as sf
-
 import config as cfg
 
 
@@ -17,28 +16,52 @@ class Audio_perpare():
 
     def read_wav(self, audio_path):
         audio_data, sampleRate = sf.read(audio_path)
+        print('audio :{0}'.format(audio_path))
         print('sample rate :{0}'.format(sampleRate))
         print('shape: {0}'.format(audio_data.shape))
         return audio_data, sampleRate
 
-    def feature_extract(self, audio_path, mfcc_bands=40, mel_spec_n_fft=640):
-        audio_data, sample_rate = self.read_wav(audio_path)
+    def _computeComplexMixtureSpectrogram(self, stereoSamples, windowSize, hopSize, windowFunction, fftSize=None):
+        if fftSize is None:
+            fftSize = windowSize
 
-        # chan_0 = audio_data[:, 0]
-        # chan_1 = audio_data[:, 1]
-        # chan_2 = audio_data[:, 2]
-        # chan_3 = audio_data[:, 3]
+        complexMixtureSpectrograms = []
+        for channelIndex in range(2):
+            complexMixtureSpectrograms.append(
+                librosa.stft(stereoSamples[i], n_fft=fftSize, hop_length=hopSize, win_length=windowSize, center=False))
+        return np.array(complexMixtureSpectrograms)
 
-        mfccs = []
-        mels = []
-        for i in range(4):
-            mfccs.append(np.mean(librosa.feature.mfcc(y=audio_data[:, i], sr=sample_rate, n_mfcc=mfcc_bands)))
-            mels.append(librosa.amplitude_to_db(librosa.feature.melspectrogram(y=audio_data[:, i], sr=sample_rate, n_fft=mel_spec_n_fft,
-                                                       hop_length=mel_spec_n_fft // 2)),ref=np.max)
+    def _getMaxTDOA(self, microphoneSeparationInMetres):
+        sound_speed = 340.0  # m/s
+        return microphoneSeparationInMetres / sound_speed
 
-        return (mfccs, mels)
+    def _getTDOAsINSecond(self, microphoneSeparationInMetres, numTDOAs):
+        max_TDOA=self._getMaxTDOA(microphoneSeparationInMetres)
+        tdoa_In_Seconds=np.linspace(-max_TDOA,max_TDOA,numTDOAs)
+        return tdoa_In_Seconds
 
-    def gcc_phat(sig, refsig, fs=1, max_tau=None, interp=16):
+
+    def _compulateAngularSpectrogram(self, complexMixtureSpectrograms, SampleRate=16000, num_TDOAs=128,
+                                     microphoneSeparationInMetres=0.05):
+        num_channel, num_frequenies, num_time = complexMixtureSpectrograms.shape
+        frequenciesInHz = np.linspace(0, SampleRate / 2, num_frequenies)
+
+        spectralCoherenceV = complexMixtureSpectrograms[0] * complexMixtureSpectrograms[1].conj() / np.abs(
+            complexMixtureSpectrograms[0]) / np.abs(complexMixtureSpectrograms[1])
+
+        tdoasInSeconds=self._getMaxTDOA(microphoneSeparationInMetres,num_TDOAs)
+        expJOmega =np.exp(np.outer(frequenciesInHz,-(2j*np.pi)*tdoasInSeconds))
+
+        FREQ, TIME, TDOA = range(3)
+        return np.sum(np.einsum(spectralCoherenceV,[FREQ, TIME],expJOmega,[FREQ, TDOA],[TDOA, FREQ, TIME] ).real,axis=1)
+
+
+
+
+
+
+
+    def gcc_phat(self, sig, refsig, fs=1, max_tau=None, interp=16):
         '''
         This function computes the offset between the signal sig and the reference signal refsig
         using the Generalized Cross Correlation - Phase Transform (GCC-PHAT)method.
@@ -67,37 +90,71 @@ class Audio_perpare():
 
         return tau, cc
 
+
+    def feature_extract(self, audio_path, mfcc_bands=40, mel_spec_n_fft=640):
+        audio_data, sample_rate = self.read_wav(audio_path)
+
+        # chan_0 = audio_data[:, 0]
+        # chan_1 = audio_data[:, 1]
+        # chan_2 = audio_data[:, 2]
+        # chan_3 = audio_data[:, 3]
+
+        mfccs = []
+        mels = []
+        for i in range(4):
+            # tt=(librosa.feature.mfcc(y=audio_data[:, i], sr=sample_rate, n_mfcc=mfcc_bands)
+            mfccs.append(librosa.feature.mfcc(y=audio_data[:, i], sr=sample_rate, n_mfcc=mfcc_bands))
+            # mfccs.append(np.mean(librosa.feature.mfcc(y=audio_data[:, i], sr=sample_rate, n_mfcc=mfcc_bands)))
+            mels.append(librosa.amplitude_to_db(
+                librosa.feature.melspectrogram(y=audio_data[:, i], sr=sample_rate, n_fft=mel_spec_n_fft,
+                                               hop_length=mel_spec_n_fft // 2), ref=np.max))
+
+        return (mfccs, mels)
+
+
     def save_feature(self, dataset_dir="DCASE2018-task5-dev", feature_dir_name='features'):
-        feature_dir = os.path.join(dataset_dir, feature_dir_name)
-        if not os.path.exists(feature_dir):
-            os.mkdir(feature_dir)
+        if not os.path.exists(feature_dir_name):
+            os.mkdir(feature_dir_name)
 
         data_meta_dirs = os.scandir(os.path.join(dataset_dir, 'evaluation_setup'))
 
         for meta in data_meta_dirs:
             try:
-                # wav_files = []
-                # label = []
-                # sess_label = []
                 with open(meta, 'r') as f_meta:
+                    # print(meta.name)
                     for line in f_meta.readlines():
+                        pass
                         print(line.split()[0])
-                        path,label,sess_label=line.split()
+                        path, label, sess_label = line.split()
 
-                        mfccs,mels=self.feature_extract(path)
+                        mfccs, mels = self.feature_extract(os.path.join(*[dataset_dir, *path.split('/')]))
+                        feature_dict = {
+                            'label': label,
+                            'session': sess_label,
+                            'mfcc': mfccs,
+                            'mel': mels
+                        }
 
+                        with open(os.path.join(*[feature_dir_name,
+                                                 meta.name.split('.txt')[0] + '-' + path.split('/')[1].split('.wav')[
+                                                     0] + '-' + label]), 'wb') as f:
+                            pickle.dump(feature_dict, f)
+                        print('dumping ' + os.path.join(*[feature_dir_name, meta.name.split('.txt')[0] + '-' +
+                                                          path.split('/')[1].split('.wav')[0] + '-' + label]))
 
             except Exception as e:
                 print(e)
 
 
-        # rootPath = os.getcwd()
-
-
 if __name__ == "__main__":
     test_solution = Audio_perpare()
-    # test_audio = ""
-    #
-    # test_solution.read_wav(test_audio)
-    test_solution.save_feature()
+    # test_solution.save_feature()
+
+    # phat test
+    refsig = np.linspace(1, 10, 10)
+
+    for i in range(0, 10):
+        sig = np.concatenate((np.linspace(0, 0, i), refsig, np.linspace(0, 0, 10 - i)))
+        offset, _ = test_solution.gcc_phat(sig, refsig)
+        print(offset)
     pass
